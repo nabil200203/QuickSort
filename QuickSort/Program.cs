86680c6a6051de5d327fe4ce707ec0bb6e32e61a
TC1009 <m.nabil.ft21@mail.umy.ac.id>
2022-10-17 17:05:39 +0700

    add number elements in array

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -7,5 +7,9 @@
         private int cmp_count = 0; // number of comparasion
         private int mov_count = 0; // number of data movements
 
+
+        // number of elements in array
+        private int n;
+
     }
 }
