c6e8190137b840c00170e2cfdeb3018dea175029
TC1009 <m.nabil.ft21@mail.umy.ac.id>
2022-10-17 17:03:51 +0700

    add class program

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -1,4 +1,11 @@
 ﻿namespace QuickShort
 {
     class program
+    {
+        //array of integers to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0; // number of comparasion
+        private int mov_count = 0; // number of data movements
+
+    }
 }
