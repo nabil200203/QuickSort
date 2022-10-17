c13573c05e47b5ea9bff03593915be08f60e6cd2
TC1009 <m.nabil.ft21@mail.umy.ac.id>
2022-10-17 20:27:43 +0700

    add number of comparison and data movemenets

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -105,9 +105,24 @@
             //sort the list on the right of pivot using quick sort
             q_sort(j + 1, high);
         }
+        void display()
+        {
+            Console.WriteLine("\n-------------------");
+            Console.WriteLine("Sorted array elments");
+            Console.WriteLine("\n-------------------");
 
+            for (int j = 0; j < n; j++)
+            {
+                Console.WriteLine(arr[j]);
 
+            }
+            Console.WriteLine("\nNumber of comparisons: ", cmp_count);
+            Console.WriteLine("\nNumber of data movemenets: " + mov_count);
 
         }
 
+
+
     }
+
+}
